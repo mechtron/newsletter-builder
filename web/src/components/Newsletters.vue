4df9d2c2ac23763049f48a94bc29a4563f32ecec
Corey <mechtrondev@gmail.com>
2020-05-26 21:41:33 -0700

    Add functionality to edit newsletters

--- web/src/components/Newsletters.vue
+++ web/src/components/Newsletters.vue
@@ -20,11 +20,11 @@
         <b-row class="justify-content-md-center">
             <b-col col lg="1"></b-col>
             <b-col cols="12" md="auto">
-                <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+                <b-form @submit="onSubmit" v-if="show">
 
                     <b-form-group
                     id="input-group-1" label-for="input-1">
-                        <b-input-group prepend="Issue #" class="mt-3">
+                        <b-input-group readonly prepend="Issue #" class="mt-3">
                             <b-form-input
                             id="input-1"
                             v-model="form.issue_number"
@@ -93,17 +93,20 @@
             0: {
                 version: "1.1.0",
                 date: "2020-04-15",
-                intro: "Welcome to issue #0"
+                intro: "Welcome to issue #0",
+                articles: []
             },
             1: {
                 version: "1.0.0",
                 date: "2020-04-30",
-                intro: "Welcome to issue #1"
+                intro: "Welcome to issue #1",
+                articles: []
             },
-            4: {
+            3: {
                 version: "0.1.0",
                 date: "2020-05-15",
-                intro: "Welcome to issue #2"
+                intro: "Welcome to issue #2",
+                articles: []
             }
         },
         selected_newsletter: 0
@@ -112,7 +115,10 @@
     methods: {
       onSubmit(evt) {
         evt.preventDefault()
-        alert(JSON.stringify(this.form))
+        this.newsletters[this.selected_newsletter].version = this.form.version
+        this.newsletters[this.selected_newsletter].date = this.form.date
+        this.newsletters[this.selected_newsletter].intro = this.form.intro
+        alert("Issue #" + this.selected_newsletter + " successfully updated")
       },
       onReset(evt) {
         evt.preventDefault()
@@ -129,7 +135,6 @@
       },
       newsletterSelected(evt) {
         evt.preventDefault()
-        console.log(evt.toElement.text)
         var issue_number = evt.toElement.text.split("#")[1]
         this.form.issue_number = issue_number
         this.selected_newsletter = issue_number
