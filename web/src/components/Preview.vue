7b995904e96fdb9f037da404f469a809bcbaefd0
Corey <mechtrondev@gmail.com>
2020-06-01 00:13:13 -0700

    Implement 'Export Data' button functionality

--- web/src/components/Preview.vue
+++ web/src/components/Preview.vue
@@ -1,13 +1,24 @@
 <template>
   <div>
     <b-container class="bv-example-row">
+      <b-container class="bv-example-row">
+          <b-row>
+              <b-col>
+                <b-button variant="outline-primary" @click="importData">Import Data</b-button>
+              </b-col>
+              <b-col> 
+                <b-button variant="outline-primary" @click="exportData">Export Data</b-button>
+              </b-col>
+              <b-col>
+                  <b-button id="generate-button" variant="success" @click="generateNewsletter">
+                  Generate <b-icon icon="play-fill"></b-icon>
+                  </b-button>
+              </b-col>
+          </b-row>
+      </b-container>
       <b-row class="justify-content-md-center">
           <b-col col lg="1"></b-col>
           <b-col cols="12">
-            <b-button id="generate-button" variant="success" @click="generateNewsletter">
-            Generate <b-icon icon="play-fill"></b-icon>
-            </b-button>
-            <br/>
             <b-form-textarea
               id="textarea"
               v-model="newsletterMarkup"
@@ -46,14 +57,30 @@
     methods: {
       setNewsletterMarkup: mutations.setNewsletterMarkup,
       setNewsletterMarkupLastUpdated: mutations.setNewsletterMarkupLastUpdated,
+      updateLastUpdated() {
+        var local_time = new Date().toLocaleTimeString()
+        this.setNewsletterMarkupLastUpdated(local_time)
+      },
       generateNewsletter(evt) {
         evt.preventDefault()
         this.setNewsletterMarkup(JSON.stringify(this.newsletters))
         this.updateLastUpdated()
       },
-      updateLastUpdated() {
-        var local_time = new Date().toLocaleTimeString()
-        this.setNewsletterMarkupLastUpdated(local_time)
+      importData(evt) {
+        evt.preventDefault()
+      },
+      downloadJsonFile(json, name) {
+        const a = document.createElement('a');
+        a.href = URL.createObjectURL(new Blob([json], { type: "text/json" }));
+        a.download = name;
+        a.click();
+      },
+      exportData(evt) {
+        evt.preventDefault()
+        this.generateNewsletter(evt)
+        var exported_data = JSON.stringify(this.newsletters)
+        var exported_filename =  "newsletter-data-" + Date.now() + ".json"
+        this.downloadJsonFile(exported_data, exported_filename)
       }
     }
   }
