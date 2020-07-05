dfe8318e565da78a00c025902ad37912e5fbb693
Corey <mechtrondev@gmail.com>
2020-07-04 17:04:52 -0700

    Enable CORS for /generate-newsletter via Flask-Cors

--- api/app.py
+++ api/app.py
@@ -3,6 +3,7 @@ import os
 from pathlib import Path
 
 from flask import Flask, send_file, request
+from flask_cors import CORS
 
 from newsletter_images import (
     download_newsletter_images,
@@ -12,6 +13,7 @@ from newsletter_images import (
 
 
 app = Flask(__name__)
+CORS(app, resources={r"/generate-newsletter": {"origins": "*"}})
 
 
 @app.route("/generate-newsletter", methods=["POST"])
@@ -24,7 +26,12 @@ def generate_newsletter():
     zip_path = zip_newsletter_images(downloaded_images_path)
     delete_temporary_folder(downloaded_images_path)
     zip_file_name = Path(zip_path).name
-    return send_file(zip_path, as_attachment=True, attachment_filename=zip_file_name)
+    return send_file(
+        zip_path,
+        mimetype="application/zip",
+        as_attachment=True,
+        attachment_filename=zip_file_name,
+    )
 
 
 if __name__ == "__main__":
